423fa85d60e016f04d0d6dbe278cc32bb85acf66
JMarts32 <j.santosa@uniandes.edu.co>
2023-09-24 21:07:54 -0500

    Reports api finished

--- safe_backend/safe_backend/settings.py
+++ safe_backend/safe_backend/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'user_api.apps.UserApiConfig'
+    'user_api.apps.UserApiConfig',
+    'reports_api.apps.ReportsApiConfig',
 ]
 
 MIDDLEWARE = [
