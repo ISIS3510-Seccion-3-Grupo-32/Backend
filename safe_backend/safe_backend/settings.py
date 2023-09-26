0f70be688ac192bee553a60dd2b6ba009ddc9e12
JMarts32 <j.santosa@uniandes.edu.co>
2023-09-26 15:04:26 -0500

    New dataBase

--- safe_backend/safe_backend/settings.py
+++ safe_backend/safe_backend/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'safe_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PW'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': 5432,
     }
 }
 
