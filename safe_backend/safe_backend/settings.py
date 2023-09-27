4904424eb8499cfa14e692d3d2b643703b756d32
JMarts32 <j.santosa@uniandes.edu.co>
2023-09-27 08:28:58 -0500

    Database Conf

--- safe_backend/safe_backend/settings.py
+++ safe_backend/safe_backend/settings.py
@@ -10,8 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import dotenv
 from pathlib import Path
 import os
+
+dotenv.load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
