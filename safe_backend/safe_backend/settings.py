66558ef9002b2edaa316435a5ee1c3a7568cb017
JMarts32 <j.santosa@uniandes.edu.co>
2023-09-27 12:15:08 -0500

    missing credentials

--- safe_backend/safe_backend/settings.py
+++ safe_backend/safe_backend/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-)u^xsyny&)-61n*t%%e1q1@9gfj4^ya#zq482qt7zj)@77hb^%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
